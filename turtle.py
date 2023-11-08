9f38181b4f64dcc49980ef2ff2aae80389f64c0e
kmj070402 <113704004+kmj070402@users.noreply.github.com>
2023-11-08 14:45:06 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,52 @@
+import turtle 
+import turtle as t
+
+
+
+#기본 설정
+t.showturtle()
+t.shape("turtle")
+t.speed(0)
+screen = t.Screen()
+screen.bgcolor("white")
+t.color("black")
+
+#도형 그리기
+
+#사각형
+#t.fillcolor("blue")
+#t.begin_fill()
+# for i in range(4):
+#     t.forward(100)
+#     t.right(90)
+#t.end_fill()
+#screen.mainloop()
+
+#원
+# t.fillcolor("blue")
+# t.begin_fill()
+# for i in range(360):
+#    t.forward(1)
+#    t.right(1)
+# t.end_fill()
+# screen.mainloop()
+
+#삼각형
+# t.fillcolor("blue")
+# t.begin_fill()
+# for i in range(3):
+#     t.forward(100)
+#     t.right(120)
+# t.end_fill()
+# screen.mainloop()
+
+#오각형
+# t.fillcolor("blue")
+# t.begin_fill()
+# for i in range(5):
+#     t.forward(100)
+#     t.right(72)
+# t.end_fill()
+# screen.mainloop()
+
+
